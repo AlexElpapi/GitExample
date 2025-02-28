b9d3ea1b97a8f6f2406d155d791dee81db535e9d
alexespinoza <alexespinoza@localhost>
2025-02-28 16:13:33 +0100

    Prova virus2

---
+++ ProvaVirus/src/Virus1/Ransomware.java
@@ -0,0 +1,11 @@
+package Virus1;
+
+public class Ransomware {
+	public static void main(String args[]) {
+		
+		System.out.println("*******************");
+		System.out.println("*QUESTO NON E' UN VIRUS*");
+		System.out.println("*******************");
+	}
+
+}
