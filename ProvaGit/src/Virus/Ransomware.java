6cdc51cfaf5fb31bdfa994bca1bded397e30699f
alexespinoza <alexespinoza@localhost>
2025-02-28 16:10:02 +0100

    Primo virus

---
+++ ProvaGit/src/Virus/Ransomware.java
@@ -0,0 +1,13 @@
+package Virus;
+
+public class Ransomware {
+
+	public static void main(String args[]) {
+		
+		System.out.println("*********************");
+		System.out.println("*********************");
+		System.out.println("*Buendos dias prof*");
+		System.out.println("*********************");
+		System.out.println("*********************");
+	}
+}
